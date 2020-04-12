63ad07ec5ee4942e96a09cf28196f23bcff19d64
Kazuki <family.computer59@gmail.com>
2020-04-12 10:04:02 +0000

    check offline

--- src/content.js
+++ src/content.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const ContentLogo = 'https://chojugiga.com/c/choju57_0019/s512_choju57_0019_0.png'
-
-const Content = () => (
-  <p>
-    <img src={ContentLogo} width={"200px"} />
-  </p>
-);
+const Content = (props) => {
+  const { logo } = props;
+  return (
+    <div>
+      <img src={logo} width={"200px"} />
+    </div>
+  );
+}
 
 export default Content;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ import Content from './content.js';
 
 const port = process.env.PORT || 3000;
 
+const ContentLogo = 'https://chojugiga.com/c/choju57_0019/s512_choju57_0019_0.png'
+const ContentLogo2 = 'https://chojugiga.com/c/choju57_0019/s512_choju57_0019_1.png'
+
 class App extends React.Component {
+  constructor(props) { 
+    super(props)
+    this.state = { 
+      logo: ContentLogo,
+      flag: false 
+    }
+  }
+  onClick(e) {
+    if (this.state.flag) {
+      this.setState( {logo: ContentLogo} )
+    } else {
+      this.setState( {logo: ContentLogo2} )
+    }
+    this.setState( {flag: !this.state.flag} )
+  }
   render () {
     return (
-        <div>
-            <Content/>
+        <div onClick = { this.onClick.bind(this) }>
+            <Content logo ={ this.state.logo }/>
         </div>
     );
   }
